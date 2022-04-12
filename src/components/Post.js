1d26dfedf67bd0b0b8d14ad4b6d09def72b76dbb
GenossenCoder <80360252+GenossenCoder@users.noreply.github.com>
2022-04-12 16:05:37 +0200

    h

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,18 +2,23 @@ import React from 'react'
 import {AiFillHeart} from 'react-icons/ai';
 
 const Post = (props) => {
+  console.log(props.date)
   return (
     <div className=" p-2 bg-white text-black overflow-hidden max-h-96 shadow-lg">
-      <h1 className="text-left ml-3 mt-3 font-bold text-black border-blue-400 border-b-2">{props.title}</h1>
-      <h2 className="text-right font-bold text-black">{props.date}</h2>
-      <h3 className="text-right font-bold text-black">{props.author}</h3>
+      <h1 className="text-left ml-3 mt-3 font-bold text-black border-blue-400 border-b-2 text-xl">{props.title}</h1>
+      <h2 className="text-right font-bold text-black text-xs">{props.date}</h2>
+      <h3 className="text-right  text-emerald-300">{props.author}</h3>
       <div className="m-1 rounded-md text-black  break-words max-h-60 overflow-hidden p-2">
         {props.content}
       </div>
       <div className="flex justify-around">
-      <a className="underline">{props.Comments}</a>
+      <a className="underline">
+      {props.comments===0&& <h1>Comment!</h1>}
+      {props.comments===1&& <h1>{props.comments} Comment</h1>}
+      {props.comments>1&& <h1>{props.comments} Comments</h1>}
+      </a>
       <div className="flex">
-        <p className="m-1">{props.like}</p>
+        <p className="m-1">{props.like} </p>
         <button><AiFillHeart color="red"/></button>
       </div>
       </div>
